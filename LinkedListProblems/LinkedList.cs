09d0b3812e858d6e85e5f8ade291369a454040f1
ravikishorereddy <ravikishorereddy309@gmail.com>
2022-03-24 23:21:45 +0530

    Welcome to LinkedList Problems

---
+++ LinkedListProblems/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    internal class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+        
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+        //Method to Add Node in Linked List
+        public void AddNode(Node node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        public void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine("linked list as follows");
+
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                {
+                    Console.Write(" ->");
+                }
+                temp = temp.next;
+            }
+        }
+    }
+}
+
---
+++ LinkedListProblems/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Node node_1 = new Node(20);
+            Node node_2 = new Node(21);
+            Node node_3 = new Node(22);
+
+            linkedList.AddNode(node_1);
+            linkedList.AddNode(node_2);
+            linkedList.AddNode(node_3);
+
+            linkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
